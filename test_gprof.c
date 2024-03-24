1e00e82c28ddba1594246ea4aabc94035d53c293
Francisco Oberto <franobertozarazaga@gmail.com>
2024-03-24 18:23:10 -0300

    [main] Profiling

--- test_gprof.c
+++ test_gprof.c
@@ -1,5 +1,6 @@
 //test_gprof.c
 #include<stdio.h>
+#include <time.h>
 
 void new_func1(void);
 
@@ -25,12 +26,22 @@ static void func2(void)
 
 int main(void)
 {
+    // Obtener el tiempo de inicio
+    clock_t inicio = clock();
     printf("\n Inside main()\n");
     int i = 0;
 
     for(;i<0xfffff11;i++);
     func1();
     func2();
+
+   // Obtener el tiempo de finalización
+    clock_t final = clock();
+
+    // Calcular el tiempo de ejecución en segundos
+    double tiempoEjecucion = (double)(final - inicio) / CLOCKS_PER_SEC;
+
+    printf("Tiempo de ejecución: %f segundos\n", tiempoEjecucion);
 
     return 0;
 }
