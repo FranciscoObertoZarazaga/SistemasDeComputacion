5720a66d1fc53d6dcee8e5ed5dbf55b0ff7552e9
Francisco Oberto <franobertozarazaga@gmail.com>
2024-04-12 18:46:38 -0300

    [TP2] Assembler

--- convert.c
+++ convert.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int convertirFloatAInt(float numero) {
-    int resultado = (int)numero;
-    return resultado + 1;
+    int resultado;
+    asm(
+        "cvttss2si %1, %%eax\n"  // Convierte el float a int en eax, truncando los decimales
+        "addl $1, %%eax\n"        // Suma 1 al valor entero
+        "movl %%eax, %0\n"        // Mueve el resultado a la variable 'resultado'
+        : "=r" (resultado)        // salida en un registro
+        : "m" (numero)            // entrada
+        : "eax"                   // clobbers
+    );
+    return resultado;
 }
 
 int main(int argc, char *argv[]) {
